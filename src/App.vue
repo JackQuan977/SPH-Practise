9abb8afe8a99d7c67736b2a9de826703d6fa812e
quanli <nuistql@163.com>
2023-07-26 23:31:30 +0800

    找到上面有空白的原因

--- src/App.vue
+++ src/App.vue
@@ -15,18 +15,10 @@ export default {
   name: "App",
   components: {
     Header,
-    Footer,
+    Footer
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
